c55450deae1f9de7671592c788281b0c845eb348
David <davidpb90@gmail.com>
2018-09-06 01:35:53 +0200

    automated the R script and added the first 4 books proposed

--- 02_Analysis/first_draft.R
+++ 02_Analysis/first_draft.R
@@ -1,42 +1,88 @@
-library(readr)
-library(devtools)
-library(stringr)
-library(gutenbergr)
-library(tidyr)
-library(tidytext)
-adventures <- read_lines("https://ia800501.us.archive.org/27/items/theadventuresofs01661gut/pg1661.txt", skip = 65) 
-#%>% 
- # data_frame()
 
-adventures <- adventures[1:(length(adventures) - 364)]
-adventures <- adventures[!is.na(adventures)]
-#trial <- as_tibble(adventures) #%>%
-#  str_replace_all("[^[:alpha:]| ]"," ") #%>% 
-#  str_split(" ")
-#trial <- adventures %>% str_extract_all(pattern = "([:alpha:]| )*") %>% str_split(" ")
-#trial <- str_split(adventures," ")
-#trial <- str_replace_all(adventures,"[^[:alpha:]| ]"," ") %>% 
-#  str_split(" ") #%>% 
-#  sapply(function(words){return(words[words!=""])},simplify = "array") #%>% 
-#  data_frame #%>% 
-  #as.tibble(names=c("lines"))
-#line<-function(line)(return(list(line[1])))
-#text_df <- data_frame(line = 1:length(adventures), text = adventures)
-#text_df %>%
-#  unnest_tokens(word, text)
+packages <- c("readr", "devtools","stringr", "tidyr", "tidytext","devtools","dplyr","rstudioapi")
+for(i in packages) {
+  if(!require(i,character.only = TRUE)) install.packages(i)
+  library(i,character.only = TRUE)
+}
+rm(packages, i)
 
-tidy_book <- adventures %>% 
-  data_frame(text=.) %>% 
-  mutate(linenumber = row_number(),chapter = cumsum(str_detect(text, regex("^ADVENTURE",ignore_case = TRUE)))) %>% 
-  unnest_tokens(word,text)
+set_wd <- function() {
+  #library(rstudioapi) # make sure you have it installed
+  current_path <- getActiveDocumentContext()$path 
+  setwd(dirname(current_path ))
+  print( getwd() )
+}
+set_wd()
 
-data(stop_words)
 
-tidy_book <- tidy_book %>%
-  anti_join(stop_words)
+read_book <- function(link, skip_init_lines = 30,skip_final_lines = 364){
+  book <- read_lines(link, skip = skip_init_lines) %>% 
+    .[1:(length(.)-skip_final_lines)] %>% #Drops the final lines of the book
+    na.omit()
+  return(book)
+}
 
-nrcjoy <- get_sentiments("nrc")
-sentiment_book <- tidy_book %>% 
-  left_join(nrcjoy)
-write.csv(sentiment_book,file = "~/Desktop/piping.csv")
+tidy_book <- function(book,chapter_regex){
+  tidied_book <- book %>% 
+    data_frame(text=.) %>% 
+    {if(chapter_regex != "") mutate(.,chapter = cumsum(str_detect(.$text, regex(chapter_regex,ignore_case = TRUE)))) else .} %>% 
+    mutate(linenumber = row_number()) %>% 
+    unnest_tokens(word,text) %>% 
+    anti_join(stop_words)
+  return(tidied_book)
+}
+
+add_sentiments <- function(tidy_book,sentiment_dict){
+  sentiments <- get_sentiments(sentiment_dict)
+  sentiment_book <- tidy_book %>% 
+  left_join(sentiments)
+  return(sentiment_book)
+}
+
+process_book <- function(link_adventures, skip_init_lines, skip_final_lines, chapter_regex, sentiment_dict, name){
+  processed_book <- read_book(link_adventures, skip_init_lines,skip_final_lines) %>% 
+    tidy_book(chapter_regex) %>% 
+    add_sentiments(sentiment_dict) 
+  write.csv(processed_book,file = paste(getwd(),"/Books/",name,".csv",sep=""))
+  return(processed_book)
+}
+  
+name <- "adventures_sherlock"
+link_adventures <- "https://ia800501.us.archive.org/27/items/theadventuresofs01661gut/pg1661.txt"
+skip_init_lines <- 57 
+skip_final_lines <- 364
+chapter_regex <- "^ADVENTURE" 
+sentiment_dict <- "nrc"
+processed_adventures <- process_book(link_adventures, skip_init_lines, skip_final_lines, chapter_regex, sentiment_dict, name)
+
+
+name <- "flatland"
+link_flatland <- "http://www.gutenberg.org/cache/epub/97/pg97.txt"
+chapter_regex <- "^SECTION"
+skip_init_lines <- 60 
+processed_flatland <- process_book(link_flatland, skip_init_lines, skip_final_lines, chapter_regex, sentiment_dict, name)
+
+name <- "kant"
+link_kant <- "https://www.gutenberg.org/files/4280/4280-0.txt"
+skip_init_lines <- 2000
+chapter_regex <- ""
+processed_kant <- process_book(link_kant, skip_init_lines, skip_final_lines, chapter_regex, sentiment_dict, name)
+
+name <- "kafka"
+link_kafka <- "http://www.gutenberg.org/cache/epub/5200/pg5200.txt"
+skip_init_lines <- 43
+chapter_regex <- ""
+processed_kant <- process_book(link_kafka, skip_init_lines, skip_final_lines, chapter_regex, sentiment_dict, name)
+
+
+
+#adventures <- read_book(link_adventures, skip_init_lines,skip_final_lines)
+#tidied_book <- tidy_book(adventures, chapter_regex)
+#sentiment_book <- add_sentiments(tidy_book,sentiment_dict)
+
+
+
+
+
+
 
